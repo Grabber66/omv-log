0052deb53fd24aeb013262e59b2fb7111725323b
Grabber66 <Grabber66@users.noreply.github.com>
2017-11-15 11:20:38 +0100

    Update tvheadend.js

--- var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/tvheadend.js
+++ var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/tvheadend.js
@@ -1,73 +1,59 @@
 /**
- * This file is part of OpenMediaVault.
- *
  * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
  * @author    Volker Theile <volker.theile@openmediavault.org>
- * @copyright Copyright (c) 2009-2017 Volker Theile
+ * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2013-2016 OpenMediaVault Plugin Developers
  *
- * OpenMediaVault is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * OpenMediaVault is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-// require("js/omv/grid/column/WhiteSpace.js")
+// require("js/omv/PluginManager.js")
 // require("js/omv/module/admin/diagnostic/log/plugin/Plugin.js")
 
-/**
- * @class OMV.module.admin.diagnostic.log.plugin.tvheadend
- * @derived OMV.module.admin.diagnostic.log.plugin.Plugin
- * Class that implements the 'tvheadend' logfile diagnostics plugin.
- */
-Ext.define("OMV.module.admin.diagnostic.log.plugin.TvHeadend", {
-	extend: "OMV.module.admin.diagnostic.log.plugin.Plugin",
-	alias: "omv.plugin.diagnostic.log.tvheadend",
-	requires: [
-		"OMV.grid.column.WhiteSpace"
-	],
+Ext.define("OMV.module.admin.diagnostic.log.plugin.AutoShutdown", {
+    extend : "OMV.module.admin.diagnostic.log.plugin.Plugin",
+    alias  : "omv.plugin.diagnostic.log.autoshutdown",
 
-	id: "tvheadend",
-	text: _("TvHeadend"),
-	stateful: true,
-	stateId: "7ec7fd4b-5b62-47fb-bd4a-aeb0e134f748",
-	columns: [{
-		text: _("Date & Time"),
-		sortable: true,
-		dataIndex: "rownum",
-		stateId: "date",
-		renderer: function(value, metaData, record) {
-			return record.get("date");
-		}
-	},{
-		xtype: "textcolumn",
-		text: _("Hostname"),
-		hidden: true,
-		sortable: true,
-		dataIndex: "hostname",
-		stateId: "hostname"
-	},{
-		xtype: "whitespacecolumn",
-		text: _("Message"),
-		sortable: true,
-		dataIndex: "message",
-		stateId: "message",
-		flex: 1
-	}],
-	rpcParams: {
-		id: "tvheadend"
-	},
-	rpcFields: [
-		{ name: "rownum", type: "int" },
-		{ name: "ts", type: "int" },
-		{ name: "date", type: "string" },
-		{ name: "hostname", type: "string" },
-		{ name: "message", type: "string" }
-	]
+    id       : "autoshutdown",
+    text     : _("TvHeadend"),
+    stateful : true,
+    stateId  : "92a5f193-e76a-481e-a1c0-12db308c97c0",
+    columns  : [{
+        text      : _("Date & Time"),
+        sortable  : true,
+        dataIndex : "date",
+        stateId   : "date",
+        renderer  : OMV.util.Format.localeTimeRenderer()
+    },{
+        text      : _("Type"),
+        sortable  : true,
+        dataIndex : "type",
+        stateId   : "type",
+        flex      : 1
+    },{
+        text      : _("Log"),
+        sortable  : true,
+        dataIndex : "log",
+        stateId   : "log",
+        flex      : 1
+    }],
+    rpcParams : {
+        id : "autoshutdown"
+    },
+    rpcFields : [
+        { name : "date", type : "string" },
+        { name : "type", type : "string" },
+        { name : "log", type : "string" }
+    ]
 });
